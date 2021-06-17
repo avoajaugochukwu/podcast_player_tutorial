36037c9fed40fe979b2ca8bcac5a93005e8e223f
avoajaugochukwu <karlthegoo@hotmail.com>
2021-06-17 21:03:54 +0100

    Checkpoint_02

---
+++ Checkpoint_02/src/containers/Spinner/Loading.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+  )
+}
+
+export default Loading
